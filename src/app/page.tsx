9de91de1f2e0ae37178606cf76db52c1be219496
roifjcf <roifjcf@gmail.com>
2025-10-08 03:31:53 +1100

    feat: disable autoplay on page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,6 @@ export default function Home() {
             ? tracks[currentTrack].url
             : undefined
         }
-        autoPlay
         preload="none"
         onEnded={handleNext}
       />
